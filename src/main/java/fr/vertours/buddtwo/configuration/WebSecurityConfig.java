61f035a847363575f588dd8935913b21cb8e9dba
VertOurs <vertours.dev@gmail.com>
2022-02-01 16:56:29 +0100

    Index opérationnelle, reste à s'occuper de du mapping des deux bouton pour login et register coté controller

--- src/main/java/fr/vertours/buddtwo/configuration/WebSecurityConfig.java
+++ src/main/java/fr/vertours/buddtwo/configuration/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,6 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        String[] resources = new String[] {
+                "/","index", "/css/**", "/images/**", "/registration"};
 
 //******************ACCEES CONSOLE H2**********************************************
 
@@ -39,17 +42,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/registration").permitAll()
+                .antMatchers(resources).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                //.loginPage("/login")
+                .loginPage("/login")
                 .permitAll()
                 .defaultSuccessUrl("/home", true);
 
-
-
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
---
+++ src/main/java/fr/vertours/buddtwo/controller/IndexController.java
@@ -0,0 +1,16 @@
+package fr.vertours.buddtwo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping
+    public ModelAndView showBuddyIntexPage() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+}
