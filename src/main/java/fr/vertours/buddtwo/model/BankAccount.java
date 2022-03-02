6a6195efacd3e21d4f537d5aeddf9a31cf509a7f
VertOurs <vertours.dev@gmail.com>
2022-03-02 12:19:30 +0100

    mise en place de la base de donnée

--- src/main/java/fr/vertours/buddtwo/model/BankAccount.java
+++ src/main/java/fr/vertours/buddtwo/model/BankAccount.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class BankAccount {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @OneToOne(optional = false)
--- src/main/java/fr/vertours/buddtwo/model/Transfer.java
+++ src/main/java/fr/vertours/buddtwo/model/Transfer.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 public class Transfer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
 
